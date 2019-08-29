7940a0c409604db54007abe62be5160e679c0a83
Aron Tolentino <arontolentino@outlook.com>
2019-08-28 23:38:24 -0400

    Implement two-column card layout for Logs component

--- src/views/Logs.vue
+++ src/views/Logs.vue
@@ -1,65 +1,215 @@
 <template>
   <ion-content>
-    <ion-card>
-      <ion-card-header>
-        <ion-card-title>
-          INCLINE DB
-          BENCH PRESS
-        </ion-card-title>
-      </ion-card-header>
+    <ion-grid>
+      <ion-row>
+        <ion-col>
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title>
+                INCLINE DB
+                BENCH PRESS
+              </ion-card-title>
+            </ion-card-header>
 
-      <ion-card-content>
-        <div>
-          <ion-grid>
-            <ion-row class="stat-heading">
-              <ion-col>
-                <div>Set</div>
-              </ion-col>
-              <ion-col>
-                <div>Weight</div>
-              </ion-col>
-              <ion-col>
-                <div>Reps</div>
-              </ion-col>
-            </ion-row>
+            <ion-card-content>
+              <div>
+                <ion-grid>
+                  <div class="stat-heading">
+                    <ion-row>
+                      <ion-col size="4">
+                        <div>Set</div>
+                      </ion-col>
+                      <ion-col size="4">
+                        <div>Lbs</div>
+                      </ion-col>
+                      <ion-col size="4">
+                        <div>Reps</div>
+                      </ion-col>
+                    </ion-row>
+                  </div>
 
-            <ion-row class="stat-entry">
-              <ion-col>
-                <div>1</div>
-              </ion-col>
-              <ion-col>
-                <div>225</div>
-              </ion-col>
-              <ion-col>
-                <div>12</div>
-              </ion-col>
-            </ion-row>
-            <ion-row class="stat-entry">
-              <ion-col>
-                <div>2</div>
-              </ion-col>
-              <ion-col>
-                <div>225</div>
-              </ion-col>
-              <ion-col>
-                <div>12</div>
-              </ion-col>
-            </ion-row>
-            <ion-row class="stat-entry">
-              <ion-col>
-                <div>3</div>
-              </ion-col>
-              <ion-col>
-                <div>225</div>
-              </ion-col>
-              <ion-col>
-                <div>12</div>
-              </ion-col>
-            </ion-row>
-          </ion-grid>
-        </div>
-      </ion-card-content>
-    </ion-card>
+                  <div class="stat-entry">
+                    <ion-row>
+                      <ion-col>
+                        <div>1</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>225</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>12</div>
+                      </ion-col>
+                    </ion-row>
+                    <ion-row class="stat-entry">
+                      <ion-col>
+                        <div>2</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>225</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>12</div>
+                      </ion-col>
+                    </ion-row>
+                    <ion-row class="stat-entry">
+                      <ion-col>
+                        <div>3</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>225</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>12</div>
+                      </ion-col>
+                    </ion-row>
+                  </div>
+                </ion-grid>
+              </div>
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+
+        <ion-col size="6">
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title>
+                INCLINE DB
+                BENCH PRESS
+              </ion-card-title>
+            </ion-card-header>
+
+            <ion-card-content>
+              <div>
+                <ion-grid>
+                  <div class="stat-heading">
+                    <ion-row>
+                      <ion-col>
+                        <div>Set</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>Weight</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>Reps</div>
+                      </ion-col>
+                    </ion-row>
+                  </div>
+
+                  <div class="stat-entry">
+                    <ion-row>
+                      <ion-col>
+                        <div>1</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>225</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>12</div>
+                      </ion-col>
+                    </ion-row>
+                    <ion-row class="stat-entry">
+                      <ion-col>
+                        <div>2</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>225</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>12</div>
+                      </ion-col>
+                    </ion-row>
+                    <ion-row class="stat-entry">
+                      <ion-col>
+                        <div>3</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>225</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>12</div>
+                      </ion-col>
+                    </ion-row>
+                  </div>
+                </ion-grid>
+              </div>
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+
+        <ion-col size="6">
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title>
+                INCLINE DB
+                BENCH PRESS
+              </ion-card-title>
+            </ion-card-header>
+
+            <ion-card-content>
+              <div>
+                <ion-grid>
+                  <div class="stat-heading">
+                    <ion-row>
+                      <ion-col>
+                        <div>Set</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>Weight</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>Reps</div>
+                      </ion-col>
+                    </ion-row>
+                  </div>
+
+                  <div class="stat-entry">
+                    <ion-row>
+                      <ion-col>
+                        <div>1</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>225</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>12</div>
+                      </ion-col>
+                    </ion-row>
+                    <ion-row class="stat-entry">
+                      <ion-col>
+                        <div>2</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>225</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>12</div>
+                      </ion-col>
+                    </ion-row>
+                    <ion-row class="stat-entry">
+                      <ion-col>
+                        <div>3</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>225</div>
+                      </ion-col>
+                      <ion-col>
+                        <div>12</div>
+                      </ion-col>
+                    </ion-row>
+                  </div>
+                </ion-grid>
+              </div>
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
+
+    <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+      <ion-fab-button>
+        <i class="fas fa-plus"></i>
+      </ion-fab-button>
+    </ion-fab>
   </ion-content>
 </template>
 
@@ -75,6 +225,7 @@ export default {
 <style scoped>
 ion-card {
   display: block;
+  margin: 5px 5px;
 }
 
 ion-card-title {
@@ -88,18 +239,22 @@ ion-card-header {
 }
 
 ion-card-content {
-  padding: 2px;
+  padding: 0px 5px 5px 5px;
 }
 
 .stat-heading {
-  font-size: 16px;
+  font-size: 14px;
   font-weight: 500;
   text-align: center;
   color: #2699fb;
 }
 
+.stat-heading ion-col {
+  padding: 5px 0;
+}
+
 .stat-entry {
-  font-size: 15px;
+  font-size: 14px;
   text-align: center;
 }
 </style>
