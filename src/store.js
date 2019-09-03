a9114f491c7640cea36b5063a8545d0d1ea3ebd8
Aron Tolentino <arontolentino@outlook.com>
2019-09-03 17:49:15 -0400

    Create sign up form integrated with Firebase

--- src/store.js
+++ src/store.js
@@ -1,10 +1,38 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import firebase from 'firebase';
+import router from '@/router';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    user: null,
+    isAuthenticated: false
+  },
+  mutations: {
+    setUser(state, payload) {
+      state.user = payload;
+    },
+    setIsAuthenticated(state, payload) {
+      state.isAuthenticated = payload;
+    }
+  },
+  actions: {
+    signUp({ commit }, { email, password }) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(user => {
+          commit('setUser', user);
+          commit('setIsAuthenticated', true);
+          router.push('/tabs');
+        })
+        .catch(error => {
+          commit('setUser', null);
+          commit('setIsAuthenticated', false);
+          console.log(error);
+        });
+    }
+  }
 });
--- src/views/Authentication/SignUp.vue
+++ src/views/Authentication/SignUp.vue
@@ -1,8 +1,68 @@
-<template></template>
+<template>
+  <ion-content class="ion-padding">
+    <v-card class="mx-auto mb-5" max-width="344">
+      <v-toolbar dark color="primary">
+        <v-toolbar-title>Sign Up</v-toolbar-title>
+      </v-toolbar>
+      <v-card-text style="width: auto">
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field
+            name="email"
+            label="Email"
+            type="email"
+            v-model="email"
+            :rules="emailRules"
+            required
+          ></v-text-field>
+          <v-text-field
+            name="password"
+            label="Password"
+            id="password"
+            type="password"
+            required
+            v-model="password"
+            :rules="passwordRules"
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" :disabled="!valid" @click="submit">Join</v-btn>
+      </v-card-actions>
+    </v-card>
+  </ion-content>
+</template>
 
 <script>
+import firebase from 'firebase';
+
 export default {
-  name: 'SignUp'
+  name: 'SignUp',
+  data() {
+    return {
+      valid: false,
+      email: '',
+      password: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => v.length >= 6 || 'Password must be greater than 6 characters'
+      ]
+    };
+  },
+  methods: {
+    submit() {
+      if (this.$refs.form.validate()) {
+        this.$store.dispatch('signUp', {
+          email: this.email,
+          password: this.password
+        });
+      }
+    }
+  }
 };
 </script>
 
