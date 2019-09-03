29afbd37baff9f29318f9ccad4cf4cadb4e0ec3d
Aron Tolentino <arontolentino@outlook.com>
2019-09-03 11:58:10 -0400

    Set up dummy workout data

--- src/components/WorkoutCard.vue
+++ src/components/WorkoutCard.vue
@@ -1,38 +1,102 @@
 <template>
-  <v-card max-width="344" class="mx-auto">
-    <v-card-title>Flat Barbell Bench Press</v-card-title>
-    <v-divider></v-divider>
-    <v-simple-table class="text-center">
-      <thead>
-        <tr>
-          <th class="text-center">Set</th>
-          <th class="text-center">Weight</th>
-          <th class="text-center">Reps</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>225</td>
-          <td>10</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>225</td>
-          <td>10</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>225</td>
-          <td>10</td>
-        </tr>
-      </tbody>
-    </v-simple-table>
-  </v-card>
+  <div>
+    <v-card max-width="344" class="mx-auto mb-5" v-for="(workout, index) in workouts" :key="index">
+      <v-card-title>{{ workout.name }}</v-card-title>
+      <v-divider></v-divider>
+      <v-simple-table class="text-center">
+        <thead>
+          <tr>
+            <th class="text-center">Set</th>
+            <th class="text-center">Weight</th>
+            <th class="text-center">Reps</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(set, index) in workout.sets" :key="index">
+            <td>{{ set.set }}</td>
+            <td>{{ set.weight }}</td>
+            <td>{{ set.reps }}</td>
+          </tr>
+        </tbody>
+      </v-simple-table>
+    </v-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'WorkoutCard'
+  name: 'WorkoutCard',
+  data: function() {
+    return {
+      workouts: [
+        {
+          name: 'Flat Barbell Bench Press',
+          sets: [
+            {
+              set: 1,
+              weight: 225,
+              reps: 10
+            },
+            {
+              set: 2,
+              weight: 225,
+              reps: 10
+            },
+            {
+              set: 3,
+              weight: 225,
+              reps: 10
+            }
+          ]
+        },
+        {
+          name: 'Incline Bench Press',
+          sets: [
+            {
+              set: 1,
+              weight: 185,
+              reps: 10
+            },
+            {
+              set: 2,
+              weight: 225,
+              reps: 10
+            },
+            {
+              set: 3,
+              weight: 225,
+              reps: 10
+            }
+          ]
+        },
+        {
+          name: 'Dips',
+          sets: [
+            {
+              set: 1,
+              weight: 185,
+              reps: 10
+            },
+            {
+              set: 2,
+              weight: 185,
+              reps: 10
+            },
+            {
+              set: 3,
+              weight: 185,
+              reps: 10
+            }
+          ]
+        }
+      ]
+    };
+  }
 };
 </script>
+
+<style>
+v-card {
+  margin-bottom: 20px;
+}
+</style>
