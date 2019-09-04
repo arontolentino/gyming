f07806c3795b11702f0bb222c985e27f7868f81c
Aron Tolentino <arontolentino@outlook.com>
2019-09-03 22:07:51 -0400

    Revert "Refactor Firebase config files"
    This reverts commit 4a641d482e1671b857ec5062465759fbffac9364.

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,8 +1,6 @@
 import firebase from 'firebase';
-import 'firebase/firestore';
 
-// Firebase init
-const config = {
+var firebaseConfig = {
   apiKey: 'AIzaSyAILT1a9iVww71noaYH1oPs3hstMfgEg8U',
   authDomain: 'gyming-app.firebaseapp.com',
   databaseURL: 'https://gyming-app.firebaseio.com',
@@ -12,14 +10,5 @@ const config = {
   appId: '1:283119908064:web:0be69afb6f078b62'
 };
 
-firebase.initializeApp(config);
-
-// firebase utilities
-const db = firebase.firestore();
-const auth = firebase.auth();
-const currentUser = auth.currentUser;
-
-// firebase collections
-const workoutsCollection = db.collection('workouts');
-
-export { db, auth, currentUser, workoutsCollection };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
